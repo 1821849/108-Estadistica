c86645c579f0019bfd9d173c2f6d1df9e4122fe5
Daniela Victoria Cansino Rosales <danielacansino7@gmail.com>
2020-02-05 13:36:36 -0600

    actualizar

--- Clases/Script_2.R
+++ Clases/Script_2.R
@@ -5,4 +5,49 @@
 
 # Importar datos ----------------------------------------------------------
 
+BD.ebanos <- read.csv("Clases/ebanos.csv", header = TRUE)
+head(BD.ebanos)
+summary(BD.ebanos)
 
+mean(BD.ebanos$diametro)
+mean(BD.ebanos$altura)
+
+sd(BD.ebanos$diametro)
+sd(BD.ebanos$altura)
+
+
+boxplot(BD.ebanos$diametro, ylab="Diámetro (cm)", 
+        col="pink", main= "Diámetro de plántulas de Ebano")
+boxplot(BD.ebanos$altura)
+boxplot
+
+stem(BD.ebanos$diametro)
+stem(BD.ebanos$altura)
+
+
+hist(BD.ebanos$diametro, col="yellow", xlab="Diámetro (cm)", 
+     ylab = "Frecuencia", main = "Histograma de Diámetro")
+
+
+# Condicionantes ----------------------------------------------------------
+
+# ¿Cuántos individuos tienen un Diam menos que la media?
+
+sum(BD.ebanos$diametro < mean(BD.ebanos$diametro))
+sum(BD.ebanos$diametro < 25.97)
+
+
+#¿Cuántos individuos tienen un Diam mayor que la media?
+
+sum(BD.ebanos$diametro > mean(BD.ebanos$diametro))
+
+sum(BD.ebanos$diametro > 30)
+
+
+# ¿Existirá una observación igual a la media?
+
+sum(BD.ebanos$diametro == mean(BD.ebanos$diametro))
+
+#subconjunto de datos subset
+
+ebanos.30 <- subset(BD.ebanos, BD.ebanos$diametro > 30)
